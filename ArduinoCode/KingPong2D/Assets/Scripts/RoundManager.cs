4a591b04e5cd29fa0a773b46f640d882c35dea1c
pramslam <pramslam@gmail.com>
2018-12-04 15:03:22 -0800

    Initial commit

---
+++ ArduinoCode/KingPong2D/Assets/Scripts/RoundManager.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using UnityEngine.UI;
+
+using UnityEditor.Events;
+
+
+
+public class RoundManager : MonoBehaviour
+{
+
+
+
+    int rounds = 0;
+
+    public int MaxNumOfRounds = 3;
+
+    public ScoreController sc;
+
+    public AudioManager am;
+
+    public Timer tm;
+
+    public BallMovement bm;
+
+    public TextMesh winScreen;
+
+
+
+    // Use this for initialization
+
+    void Start()
+    {
+        winScreen.text = "";
+
+
+    }
+
+
+
+    // Update is called once per frame
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Backspace)) {
+            StartCoroutine(pauseForNewPlayers());
+        }
+
+    }
+
+
+
+    public void gameOver(int winningPlayer, string winningPlayerName, int losingPlayer, string losingPlayerName)
+    {
+
+            winScreen.text = "Congrats " + winningPlayerName + "! You won with " + winningPlayer + " points! /n Don't Worry " + losingPlayer + " you'll get them next time!";
+
+            StartCoroutine(pauseForNewPlayers());
+            sc.ResetScore();
+
+            tm.StartTimer();
+
+            bm.Spawn();
+        winScreen.text = "";
+
+    }
+
+    public IEnumerator pauseForNewPlayers() {
+        Time.timeScale = 0.0001f;
+        yield return StartCoroutine(waitForInput(KeyCode.Space));
+        Time.timeScale = 1;
+
+
+    }
+    IEnumerator waitForInput(KeyCode key) {
+        while (!Input.GetKeyDown(key)) {
+            yield return null;
+        }
+    }
+
+}
---
+++ ArduinoCode/KingPong2D/Assets/ToastsGifConverter/GifPlayerMesh.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GifPlayerMesh : GifPlayerInterface
+{
+    private MeshRenderer meshy;
+    public GifData _g;
+    Coroutine lastCoroutine;
+    // Use this for initialization
+    void Awake()
+    {
+        meshy = GetComponent<MeshRenderer>();
+    }
+    /*
+    public Coroutine Init(GifData gif)
+    {
+       Coroutine lc;
+       playing = true;
+       lc= StartCoroutine(play(gif));
+       return lc;
+    }
+  
+
+
+    private void Start()
+    {
+         Init(_g);   
+    }
+
+    public void ChangeGif(GifData gd) {
+        playing = false;
+        StopCoroutine(lastCoroutine);
+        lastCoroutine = Init(gd);
+    }
+      */
+
+    public override void Initialize(GifData gif)
+
+    {
+
+        playing = true;
+
+        StartCoroutine(Play(gif));
+
+    }
+
+    private void Start()
+
+    {
+
+        Initialize(_g);
+
+    }
+
+
+
+    public void ChangeGif(GifData gd)
+    {
+
+        playing = false;
+
+        //StopCoroutine(play(_g));
+
+        Initialize(gd);
+
+    }
+
+
+
+    IEnumerator Play(GifData gif)
+    {
+        _g = gif;
+        while (playing)
+        {
+            for (int j = 0; j < _g.frames.Length; j++)
+            {
+                meshy.material.mainTexture = _g.frames[j];
+                yield return new WaitForSeconds(_g.delay);
+            }
+        }
+        playing = false;
+    }
+}
